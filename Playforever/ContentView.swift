a387a880335600c0c552dae0fc19ee3a5cfeed06
qerplunk <aquinoedgar@outlook.com>
2023-10-08 19:36:19 -0700

    Xcode project files

---
+++ Playforever/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Playforever
+//
+//  Created by sam on 10/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Playforever !")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Playforever/PlayforeverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayforeverApp.swift
+//  Playforever
+//
+//  Created by sam on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct PlayforeverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
