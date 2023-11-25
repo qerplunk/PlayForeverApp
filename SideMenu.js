da0b6671cd9c78fdd3c06e10185d9090bf6fa3fa
qerplunk <aquinoedgar@outlook.com>
2023-11-25 02:48:29 -0700

    Refined side menu, added web links to logos

--- SideMenu.js
+++ SideMenu.js
@@ -1,44 +1,91 @@
 import { React } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { ImageBackground, View, Linking, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+const TextButton = ({text}) => {
+  return (
+      <TouchableOpacity
+        style={styles.textButton.button}
+      >
+        <Text style={styles.textButton.text}>
+          {text}
+        </Text>
+      </TouchableOpacity>
+  );
+}
+
+const LogoButton = ({imageURL, webURL}) => {
+  return (
+      <TouchableOpacity
+        style={styles.logoButton}
+        onPress={() => Linking.openURL(webURL)}
+      >
+        <ImageBackground
+          style={{...StyleSheet.absoluteFillObject}}
+          source={imageURL}
+        />
+      </TouchableOpacity>
+  );
+}
 
 const SideMenu = () => {
   return(
     <View
       style={{opacity: 0.93, position: 'absolute', width: 300, height: 900, backgroundColor: 'black'}}
     >
-      <TouchableOpacity
-        style={{marginTop: 50, marginLeft: 20, backgroundColor: 'black', alignSelf: 'flex-start'}}
-      >
-        <Text style={{color: 'white', fontSize: 22, fontFamily: 'Futura-Medium', padding: 4}}>
-          Archived
-        </Text>
-      </TouchableOpacity>
+      <TextButton text={'Archived'} />
+      <TextButton text={'App Settings'} />
+      <TextButton text={'Feedback'} />
+      <TextButton text={'Archived'} />
+      <TextButton text={'Help'} />
 
-      <TouchableOpacity
-        style={{marginTop: 20, marginLeft: 20, backgroundColor: 'black', alignSelf: 'flex-start'}}
-      >
-        <Text style={{color: 'white', fontSize: 22, fontFamily: 'Futura-Medium', padding: 4}}>
-          App Settings
-        </Text>
-      </TouchableOpacity>
+      <View style={{padding: 30}} />
 
-      <TouchableOpacity
-        style={{marginTop: 20, marginLeft: 20, backgroundColor: 'black', alignSelf: 'flex-start'}}
-      >
-        <Text style={{color: 'white', fontSize: 22, fontFamily: 'Futura-Medium', padding: 4}}>
-          Feedback
-        </Text>
-      </TouchableOpacity>
+      <LogoButton
+        imageURL={require('./assets/facebook_logo.png')}
+        webURL={'https://www.facebook.com/Playforever.ca/'}
+      />
+      <LogoButton
+        imageURL={require('./assets/twitter_logo.png')}
+        webURL={'https://twitter.com/playforever_ca'}
+      />
+      <LogoButton
+        imageURL={require('./assets/instagram_logo.png')}
+        webURL={'https://www.instagram.com/playforever.ca/'}
+      />
+      <LogoButton
+        imageURL={require('./assets/linkedin_logo.png')}
+        webURL={'https://www.linkedin.com/company/play-forever/?originalSubdomain=ca'}
+      />
 
-      <TouchableOpacity
-        style={{marginTop: 20, marginLeft: 20, backgroundColor: 'black', alignSelf: 'flex-start'}}
-      >
-        <Text style={{color: 'white', fontSize: 22, fontFamily: 'Futura-Medium', padding: 4}}>
-          Help
-        </Text>
-      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  textButton: {
+    button: {
+      width: 150,
+      marginTop: 20,
+      marginLeft: 20,
+    },
+    text: {
+      color: 'white',
+      fontSize: 22,
+      fontFamily: 'Futura-Medium',
+      padding: 4
+    }
+  },
+  logoButton: {
+    width: 50,
+    height: 50,
+    borderColor: 'white',
+    borderWidth: 0.5,
+    marginTop: 20,
+    marginLeft: 20,
+    backgroundColor: 'black',
+    alignSelf: 'flex-start',
+  }
+})
+
 
 export default SideMenu;
