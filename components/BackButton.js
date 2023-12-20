32e5b40d808cb883d1a14706941d540a206a5ac2
qerplunk <aquinoedgar@outlook.com>
2023-12-20 14:24:22 -0700

    Added BackButton component and style sheet

---
+++ components/BackButton.js
@@ -0,0 +1,19 @@
+import { React } from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+
+import BackButtonStyles from '../styles/BackButtonStyles';
+
+const BackButton = ({navigation, destination, text}) => {
+  return (
+    <TouchableOpacity
+      style={BackButtonStyles.container}
+      onPress={() => navigation.navigate(destination)}
+    >
+      <Text style={BackButtonStyles.text}>
+        {text ? text : 'Back'}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default BackButton;
---
+++ styles/BackButtonStyles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const BackButtonStyles = StyleSheet.create({
+  container: {
+    width: 80,
+    height: 36,
+    margin: 10,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  text: {
+    fontFamily: 'Futura-Medium',
+    fontSize: 16,
+  }
+});
+
+export default BackButtonStyles;
