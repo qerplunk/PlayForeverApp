6fde753d82b7464ca970593a0e8822bf11ad5478
qerplunk <aquinoedgar@outlook.com>
2023-12-20 23:22:31 -0700

    Removed unwanted navigation animation warning

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StatusBar } from 'react-native';
+import { StatusBar, LogBox } from 'react-native';
 
 import Home from './screens/Home';
 import MemberPage from './screens/MemberPage';
@@ -9,6 +9,10 @@ import ContactUs from './screens/ContactUs';
 import AboutUs from './screens/AboutUs';
 
 const Stack = createStackNavigator();
+
+// To ignore navigation animation warning
+// https://github.com/react-navigation/react-navigation/issues/7839
+LogBox.ignoreLogs(['Sending `onAnimatedValueUpdate` with no listeners registered.']);
 
 export default function App() {
   return (
